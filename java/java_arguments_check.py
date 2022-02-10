df2057b24196574787d15a7f23964434acacd7f3
Zakopoulos Athanasios <azakopoulos@ote.gr>
2022-02-10 11:56:41 +0200

    pre namespace arg

--- java/java_arguments_check.py
+++ java/java_arguments_check.py
@@ -13,25 +13,43 @@ from functions import look_for_other_nfs_servers
 from functions import write_to_a_file
 
 
+# Create the parser
+parser = argparse.ArgumentParser()
+
+# Add an argument
+parser.add_argument('--namespace', type=str, required=True)
+
+# Parse the argument. If argument is empty or not good put default argument 
+try:
+  args = parser.parse_args()
+  ##The usage threshold if none is given as an argument
+  ##The ip of the NFS server
+  list_of_namespaces_string  = args.namespace
+  ##stupid solution to parse namespace later correct. Need to code this in a more elegant way
+  list_of_namespaces=[list_of_namespaces_string,""]
+except:
+  ##Get a list of the namespaces. 
+  get_namespaces_command="kubectl get ns --no-headers -o jsonpath='{.items[*].metadata.name}'"
+  list_of_namespaces = output_command(get_namespaces_command)
+
+
+
 ##substring to search
 substring="MaxRAMPercentage"
 
 
-##Get a list of the namespaces. 
-get_namespaces_command="kubectl get ns --no-headers -o jsonpath='{.items[*].metadata.name}'"
-list_of_namespaces = output_command(get_namespaces_command)
-##debug##print(type(list_of_namespaces))
-##debug##print(list_of_namespaces)
-
-
 ##For all the current_namespaces
 for current_namespace in list_of_namespaces:
+  ##stupid control for case namespace argument is selected. Need to code this in a more elegant way
+  if not current_namespace:
+     continue
+
   ##counter for values bigger than threshold
   java_pods_counter = 0
   jcmd_pods_counter = 0
   ps_pod_counter = 0
   ##Get a list of pods per current_namespace
-  if current_namespace=="default" or current_namespace=="velero" or current_namespace=="anchore" or current_namespace=="filebeat" or current_namespace=="dynatrace" or current_namespace=="busybox" or current_namespace=="istio-cosmote"  or current_namespace=="istio-system" or current_namespace=="kube-system" or current_namespace=="metricbeat" or current_namespace=="monitoring"  or current_namespace=="tracing":
+  if current_namespace=="default" or current_namespace=="velero" or current_namespace=="anchore" or current_namespace=="filebeat" or current_namespace=="dynatrace" or current_namespace=="busybox" or current_namespace=="istio-cosmote"  or current_namespace=="istio-system" or current_namespace=="kube-system" or current_namespace=="metricbeat" or current_namespace=="monitoring"  or current_namespace=="tracing" or current_namespace=="kube-public" or current_namespace=="minio" or current_namespace=="kube-node-lease":
    continue
   get_po_per_current_namespace_command="kubectl -n %s  get po -o jsonpath='{.items[*].metadata.name}'"%current_namespace
   list_of_pods_per_current_namespace = output_command(get_po_per_current_namespace_command,"True")
